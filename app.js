5bede9859f8659cbe0fc60e021a6d7d4ced044f4
Stan Griffith <swgthree@gmail.com>
2020-10-28 19:26:01 -0600

    Added game play to app.js

--- app.js
+++ app.js
@@ -1,19 +1,60 @@
-let randomNum = Math.floor(Math.random() * (10 - 1) + 1);
+// Create random number between 1 and 3
+let randomNum = Math.floor(Math.random() * (4 - 1) + 1);
 
+// Asign random number to rock, paper, or sissors
 function computerPlay(randomNum) {
-  if (randomNum <= 3) {
-    randomNum = 'Rock';
+  if (randomNum === 1) {
+    randomNum = 'rock';
   }
-  if (randomNum >= 4 && randomNum <= 6) {
-    randomNum = 'Paper';
+  if (randomNum === 2) {
+    randomNum = 'paper';
   }
-  if (randomNum >= 7) {
-    randomNum = 'Sissors';
+  if (randomNum === 3) {
+    randomNum = 'sissors';
   }
   return randomNum;
 }
 
-let compPick = computerPlay(randomNum);
+// Variable holding the computers pick; rock, paper or sissors
+let computerSelection = computerPlay(randomNum);
 
 console.log(randomNum);
-console.log(compPick);
+console.log(computerSelection);
+
+// prompt asking player for input
+let playerPrompt = prompt('Battle to the death! "Rock", "Paper" or "Sissors"?');
+
+// convert player input to all lower case
+let playerSelection = playerPrompt.toLowerCase();
+
+console.log(playerSelection);
+
+// game rules and play
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return `It's a tie, you both chose ${playerSelection}`;
+  }
+  if (playerSelection == 'rock' && computerSelection == 'paper') {
+    return `Computer chose ${computerSelection} and you chose ${playerSelection}. You loose, Paper beats Rock!`;
+  }
+  if (playerSelection == 'paper' && computerSelection == 'rock') {
+    return `Computer chose ${computerSelection} and you chose ${playerSelection}. You win, Paper beats Rock!`;
+  }
+  if (playerSelection == 'rock' && computerSelection == 'sissors') {
+    return `Computer chose ${computerSelection} and you chose ${playerSelection}. You win, Rock beats Sissors!`;
+  }
+  if (playerSelection == 'sissors' && computerSelection == 'rock') {
+    return `Computer chose ${computerSelection} and you chose ${playerSelection}. You loose, Rock beats Sissors!`;
+  }
+  if (playerSelection == 'sissors' && computerSelection == 'paper') {
+    return `Computer chose ${computerSelection} and you chose ${playerSelection}. You win, Sissors beat paper!`;
+  }
+  if (playerSelection == 'paper' && computerSelection == 'sissors') {
+    return `Computer chose ${computerSelection} and you chose ${playerSelection}. You loose, Sissors beat paper!`;
+  }
+  return playRound;
+}
+
+// variable holding the round results
+let gameResults = playRound(playerSelection, computerSelection);
+console.log(gameResults);
